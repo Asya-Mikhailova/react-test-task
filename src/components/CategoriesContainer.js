75c52fca59c3d0c8448b4373a314704a045b235e
Anastasia Mikhailova <Anastasia_Mikhailova@epam.com>
2020-10-26 15:35:39 +0100

    fix: change css to sass

--- src/components/CategoriesContainer.js
+++ src/components/CategoriesContainer.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCategories, setIsSelected } from '../redux';
-import './CategoriesContainer.css';
+import './CategoriesContainer.scss';
 
 export const CategoriesContainer = () => {
   const loading = useSelector((state) => state.categories.loading);
