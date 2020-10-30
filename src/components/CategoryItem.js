4c5313903a996ee02eb649bbd167eef623d3c5e7
Anastasia Mikhailova <Anastasia_Mikhailova@epam.com>
2020-10-30 12:51:34 +0100

    feat: add material ui tooltip

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -4,7 +4,30 @@ import { setIsSelected } from '../redux';
 
 import './CategoryItem.scss';
 
-export const CategoryItem = ({ category, key }) => {
+import { withStyles } from '@material-ui/core/styles';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const CustomTooltip = withStyles({
+  tooltip: {
+    width: '380px',
+    fontSize: '14px',
+    color: 'black',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    opacity: '0.7',
+    border: '1px solid #eceeef',
+    padding: '15px',
+    margin: '-2px 0',
+    textAlign: 'center',
+  },
+  arrow: {
+    '&:before': {
+      border: '1px solid #eceeef',
+    },
+    color: 'rgba(255, 255, 255, 0.8)',
+  },
+})(Tooltip);
+
+export const CategoryItem = ({ category }) => {
   const dispatch = useDispatch();
 
   const selectCategory = (name) => {
@@ -12,21 +35,28 @@ export const CategoryItem = ({ category, key }) => {
   };
 
   return (
-    <button className='category' onClick={() => selectCategory(category.name)}>
-      <div className='category__status-icon'>
-        {category.isSelected ? (
-          <i className='fa fa-smile-o category__status-icon_success' />
-        ) : (
-          <i className='fa fa-simplybuilt category__status-icon_danger ' />
-        )}
-      </div>
+    <CustomTooltip
+      className='tooltip'
+      title={category.description}
+      arrow
+      placement='top'
+    >
+      <button
+        key={category.name}
+        className='category'
+        onClick={() => selectCategory(category.name)}
+      >
+        <div className='category__status-icon'>
+          {category.isSelected ? (
+            <i className='fa fa-smile-o category__status-icon_success' />
+          ) : (
+            <i className='fa fa-simplybuilt category__status-icon_danger ' />
+          )}
+        </div>
 
-      <p key={key}>{category.name}</p>
-      <i className='fa fa-info category__tooltip'>
-        <span className='category__tooltip_tooltiptext'>
-          {category.description}
-        </span>
-      </i>
-    </button>
+        <p>{category.name}</p>
+        <i className='fa fa-info'></i>
+      </button>
+    </CustomTooltip>
   );
 };
