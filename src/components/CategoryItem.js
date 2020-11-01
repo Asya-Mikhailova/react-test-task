ef5ed6a65a7bff25ade315ffa62a96985f7e53ab
Anastasia Mikhailova <Anastasia_Mikhailova@epam.com>
2020-11-01 16:21:21 +0100

    fix: fix tooltip styling and positioning

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -4,30 +4,30 @@ import { setIsSelected } from '../redux';
 
 import './CategoryItem.scss';
 
-import { withStyles } from '@material-ui/core/styles';
-import Tooltip from '@material-ui/core/Tooltip';
+// import { withStyles } from '@material-ui/core/styles';
+// import Tooltip from '@material-ui/core/Tooltip';
 
-//import { CustomTooltip } from './CustomTooltip';
+// //import { CustomTooltip } from './CustomTooltip';
 
-const CustomTooltip = withStyles({
-  tooltip: {
-    width: '380px',
-    fontSize: '14px',
-    color: 'black',
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
-    border: '1px solid #eceeef',
-    padding: '15px',
-    margin: '5px 0',
-    textAlign: 'center',
-  },
-  arrow: {
-    '&:before': {
-      border: '1px solid #eceeef',
-      width: '15px',
-    },
-    color: 'rgba(255, 255, 255, 0.8)',
-  },
-})(Tooltip);
+// const CustomTooltip = withStyles({
+//   tooltip: {
+//     width: '380px',
+//     fontSize: '14px',
+//     color: 'black',
+//     backgroundColor: 'rgba(255, 255, 255, 0.9)',
+//     border: '1px solid #eceeef',
+//     padding: '15px',
+//     margin: '5px 0',
+//     textAlign: 'center',
+//   },
+//   arrow: {
+//     '&:before': {
+//       border: '1px solid #eceeef',
+//       width: '15px',
+//     },
+//     color: 'rgba(255, 255, 255, 0.8)',
+//   },
+// })(Tooltip);
 
 export const CategoryItem = ({ category }) => {
   const dispatch = useDispatch();
@@ -37,32 +37,26 @@ export const CategoryItem = ({ category }) => {
   };
 
   return (
-    <CustomTooltip
-      className='tooltip'
-      title={category.description}
-      arrow={true}
-      placement='top'
+    <button
+      key={category.name}
+      className='category category__tooltip category__tooltip_top'
+      onClick={() => selectCategory(category.name)}
+      data-text={category.description}
     >
-      <button
-        key={category.name}
-        className='category'
-        onClick={() => selectCategory(category.name)}
-      >
-        <div className='category__status-icon'>
-          {category.isSelected ? (
-            <i className='fa fa-smile-o category__status-icon_success' />
-          ) : (
-            <i className='fa fa-simplybuilt category__status-icon_danger ' />
-          )}
-        </div>
+      <div className='category__status-icon'>
+        {category.isSelected ? (
+          <i className='fa fa-smile-o category__status-icon_success' />
+        ) : (
+          <i className='fa fa-simplybuilt category__status-icon_danger ' />
+        )}
+      </div>
 
-        <p>
-          {category.name}
-          <span>
-            <i className='fa fa-info' />
-          </span>
-        </p>
-      </button>
-    </CustomTooltip>
+      <p>
+        {category.name}
+        <span>
+          <i className='fa fa-info' />
+        </span>
+      </p>
+    </button>
   );
 };
