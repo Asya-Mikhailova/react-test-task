2809db7e618b8d087fbfc09de4e31b64aed4237c
Mikhailova <amikhail@krk-mp10g.munich.corp.akamai.com>
2020-11-12 13:24:04 +0100

    feat: add router to app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './redux/store';
 import './App.scss';
-import { CategoriesContainer } from './components/CategoriesContainer';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import {CategoriesContainer} from './components/CategoriesContainer';
+import {ProfilesContainer} from "./components/ProfilesContainer";
+
 
 function App() {
-  return (
-    <Provider store={store}>
-      <CategoriesContainer />
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <h1>HOME</h1>
+                    </Route>
+                    <Route path="/categories">
+                        <CategoriesContainer/>
+                    </Route>
+                    <Route path="/profiles">
+                        <ProfilesContainer/>
+                    </Route>
+                </Switch>
+
+            </Router>
+
+        </Provider>
+
+
+
+    );
 }
 
 export default App;
