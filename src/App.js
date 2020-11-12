46d805c884e250b8e1326aec77a1b7a30d90dc1c
Mikhailova <amikhail@krk-mp10g.munich.corp.akamai.com>
2020-11-12 16:46:40 +0100

    fix: changed routing according to the task

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import './App.scss';
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import {CategoriesContainer} from './components/CategoriesContainer';
 import {ProfilesContainer} from "./components/ProfilesContainer";
 
@@ -11,24 +11,10 @@ function App() {
     return (
         <Provider store={store}>
             <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <h1>HOME</h1>
-                    </Route>
-                    <Route path="/categories">
-                        <CategoriesContainer/>
-                    </Route>
-                    <Route path="/profiles">
-                        <ProfilesContainer/>
-                    </Route>
-                </Switch>
-
+                <Route exact path="/" component={CategoriesContainer}/>
+                <Route exact path="/categories/:id" component={CategoriesContainer}/>
             </Router>
-
         </Provider>
-
-
-
     );
 }
 
